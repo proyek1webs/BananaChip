f340daa608b53f1745239628c440443d379ea991
Mallq <Kamalputra1177@gmail.com>
2025-05-15 16:01:08 +0700

    href wa

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    document.getElementById('kirim-wa').addEventListener('click', function() {
+        var nama = document.getElementById('nama').value;
+        var email = document.getElementById('email').value;
+        var pesan = document.getElementById('pesan').value;
+
+        if(nama && email && pesan){
+            var text = "Halo Banana King%0ASaya ingin memberikan saran.%0ANama: " + encodeURIComponent(nama) + "%0AEmail: " + encodeURIComponent(email) + "%0APesan: " + encodeURIComponent(pesan);
+            window.open('https://wa.me/6281234567890?text=' + text, '_blank');
+        } else {
+            alert("Mohon isi semua kolom sebelum mengirim.");
+        }
+    });
